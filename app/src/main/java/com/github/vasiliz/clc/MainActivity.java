30fe07dc777fc76db7609f7fd6fef24fb82cce6c
VasiliZ <Zaicev.Vasili@gmail.com>
2018-05-02 21:47:20 +0300

    End fix

--- app/src/main/java/com/github/vasiliz/clc/MainActivity.java
+++ app/src/main/java/com/github/vasiliz/clc/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -12,81 +13,82 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final NumberFormat CURRENCY_FORMAT = NumberFormat.getNumberInstance();
-    private static final NumberFormat PERCENT_FORMAT = NumberFormat.getNumberInstance();
-
-    private double mBillAmount = 0.0;
-    private double mPercent = 0.15;
+    private double mBillAmount;
+    private double mPercent = 15;
     private TextView mAmountTextView;
     private TextView mPercentTextView;
     private TextView mTipTextView;
     private TextView mTotalTextView;
     private EditText mAmountEditText;
 
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
     }
 
-    private void init(){
+    private void init() {
         mAmountTextView = findViewById(R.id.amount_edit_text);
         mPercentTextView = findViewById(R.id.percent_text_view);
         mTipTextView = findViewById(R.id.tip_text_view);
         mTotalTextView = findViewById(R.id.total_text_view);
-        mTipTextView.setText(CURRENCY_FORMAT.format(0));
-        mTotalTextView.setText(CURRENCY_FORMAT.format(0));
+        mTipTextView.setText("0");
+        mTotalTextView.setText("0");
         mAmountEditText = findViewById(R.id.amount_edit_text);
-       // mAmountEditText.addTextChangedListener(amountEditTextWatcher);
-        SeekBar seekBar = findViewById(R.id.percent_seek_bar);
+        mAmountEditText.addTextChangedListener(amountEditTextWatcher);
+        final SeekBar seekBar = findViewById(R.id.percent_seek_bar);
+        seekBar.setProgress((int) mPercent);
         seekBar.setOnSeekBarChangeListener(seekBarListener);
     }
 
-    private void calculate(){
-        mPercentTextView.setText(PERCENT_FORMAT.format(mPercent));
+    private void calculate() {
+        mPercentTextView.setText(mPercent + "%");
 
-        double tip = 1 * mPercent;
-        double total = mBillAmount + tip;
+        final double tip = mBillAmount * mPercent / 100;
+        final double total = mBillAmount + tip;
 
-        mTipTextView.setText(CURRENCY_FORMAT.format(tip));
-        mTotalTextView.setText(CURRENCY_FORMAT.format(total));
+
+        mTipTextView.setText(String.valueOf(tip));
+        mTotalTextView.setText(String.valueOf(total));
     }
 
+    private String TAG = "log";
     private final SeekBar.OnSeekBarChangeListener seekBarListener =
             new SeekBar.OnSeekBarChangeListener() {
+
+                int progress;
+
                 @Override
-                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                    double a = i / 100;
-                    mPercent = a;
+                public void onProgressChanged(final SeekBar seekBar, final int i, final boolean b) {
+                    progress = i;
+                }
+
+                @Override
+                public void onStartTrackingTouch(final SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(final SeekBar seekBar) {
+                    mPercent = (double) progress;
                     calculate();
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
 
                 }
             };
 
     private final TextWatcher amountEditTextWatcher = new TextWatcher() {
+
         @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        public void beforeTextChanged(final CharSequence charSequence, final int i, final int i1, final int i2) {
 
         }
 
         @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            try{
-                mBillAmount = Double.parseDouble(charSequence.toString()) / 100;
-                mAmountTextView.setText(CURRENCY_FORMAT.format(mBillAmount));
-            }catch (Exception e){
+        public void onTextChanged(final CharSequence charSequence, final int i, final int i1, final int i2) {
+            try {
+                mBillAmount = Double.parseDouble(charSequence.toString());
+            } catch (final Exception e) {
                 e.fillInStackTrace();
                 mAmountTextView.setText("");
                 mBillAmount = 0.0;
@@ -96,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void afterTextChanged(Editable editable) {
+        public void afterTextChanged(final Editable editable) {
 
         }
     };
